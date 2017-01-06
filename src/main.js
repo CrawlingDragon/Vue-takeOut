0c6313881b1c07a8b44008bf3efff41455f946a1
CrawlingDragon <363630867@qq.com>
2017-01-06 15:19:26 +0800

    vue-router完成
    2017-1-6

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+//  vue-router 是插件里的name 把vue-router值赋值给VueRouter变量
+import VueRouter from 'vue-router';
+import App from './App';
+import goods from './components/goods/goods.vue';
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
-})
+Vue.use(VueRouter);
 
+//  这里的App是上文引用的App组件
+let app = Vue.extend(App);
+//  创建一个router实例
+let router = new VueRouter();
+router.map({
+  '/goods': {
+    component: goods
+  }
+});
+router.start(app, '#app');
