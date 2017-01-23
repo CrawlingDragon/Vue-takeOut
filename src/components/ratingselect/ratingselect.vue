a4045d2380deb2f64ce83bec05b37a01122cbab9
CrawlingDragon <363630867@qq.com>
2017-01-23 16:47:09 +0800

    2017-1-23 food组件
    2017-1-23 food组件

--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="ratingselect">
     <div class="rating-type border-1px">
-      <span class="block positive">{{desc.all}}<span class="count">47</span></span>
-      <span class="block positive">{{desc.positive}}<span class="count">40</span></span>
-      <span class="block negative">{{desc.negative}}<span class="count">7</span></span>
+      <span class="block positive" :class="{'active':selectType===2}" @click="select(2,$event)">{{desc.all}}<span
+        class="count">{{ratings.length}}</span></span>
+      <span class="block positive" :class="{'active':selectType===0}" @click="select(0,$event)">{{desc.positive}}<span
+        class="count">{{positives.length}}</span></span>
+      <span class="block negative" :class="{'active':selectType===1}" @click="select(1,$event)">{{desc.negative}}<span
+        class="count">{{negative.length}}</span></span>
     </div>
-    <div class="switch">
+    <div class="switch" :class="{'on':onlyContent}" @click="toggleContent">
       <span class="icon-check_circle"></span>
       <span class="text">只看有内容的评价</span>
     </div>
@@ -13,8 +16,8 @@
 </template>
 
 <script type="text/ecmascript-6">
-  //  const POSITIVE = 0;
-  //  const NEGATIVE = 1;
+  const POSITIVE = 0;
+  const NEGATIVE = 1;
   const ALL = 2;
   export default {
     props: {
@@ -42,6 +45,34 @@
           };
         }
       }
+    },
+    computed: {
+      positives() {
+        return this.ratings.filter((rating) => {
+          return rating.rateType === POSITIVE;
+        });
+      },
+      negative() {
+        return this.ratings.filter((rating) => {
+          return rating.rateType === NEGATIVE;
+        });
+      }
+    },
+    methods: {
+      select(type, event) {
+        if (!event._constructed) {
+          return;
+        }
+        this.selectType = type;
+        this.$dispatch('ratingtype.select', type);
+      },
+      toggleContent(event) {
+        if (!event._constructed) {
+          return;
+        }
+        this.onlyContent = !this.onlyContent;
+        this.$dispatch('content.toggle', this.onlyContent);
+      }
     }
   };
 </script>
@@ -54,4 +85,41 @@
       margin: 0 18px
       border-1px(rgba(7, 17, 27, 0.1))
       font-size: 0
+      .block
+        display: inline-block
+        padding: 8px 12px
+        margin-right: 8px
+        line-height: 8px
+        border-radius: 16px
+        font-size: 12px
+        color: rgb(77, 85, 93)
+        &.active
+          color: #ffffff
+        .count
+          margin-left: 2px
+          font-size: 8px
+        &.positive
+          background: rgba(0, 160, 220, 0.2)
+          &.active
+            background: rgb(0, 160, 220)
+        &.negative
+          background: rgba(77, 85, 93, 0.2)
+          &.active
+            background: rgb(77, 85, 93)
+    .switch
+      padding: 12px 18px
+      line-height: 24px
+      border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+      color: rgb(147, 153, 159)
+      font-size: 0
+      &.on
+        .icon-check_circle
+          color: #00c850
+      .icon-check_circle
+        display: inline-block
+        vertical-align: top
+        margin-right: 4px
+        font-size: 24px
+      .text
+        font-size: 12px
 </style>
